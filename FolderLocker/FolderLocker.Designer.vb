ee2bde9842cf25c07954fa054d04d61106bf959e
Brandon Taylor <bt610490@gmail.com>
2017-12-23 22:29:33 -0600

    Add files via upload

---
+++ FolderLocker/FolderLocker.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmFolderLocker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmFolderLocker))
+        Me.lblPrompt = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblPrompt
+        '
+        Me.lblPrompt.AutoSize = True
+        Me.lblPrompt.Location = New System.Drawing.Point(12, 9)
+        Me.lblPrompt.Name = "lblPrompt"
+        Me.lblPrompt.Size = New System.Drawing.Size(158, 13)
+        Me.lblPrompt.TabIndex = 0
+        Me.lblPrompt.Text = "Enter password to Unlock folder"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(15, 25)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(257, 20)
+        Me.txtPassword.TabIndex = 1
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(197, 51)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 2
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Location = New System.Drawing.Point(116, 51)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 3
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmFolderLocker
+        '
+        Me.AcceptButton = Me.btnOK
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancel
+        Me.ClientSize = New System.Drawing.Size(284, 86)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.lblPrompt)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmFolderLocker"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Folder Locker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblPrompt As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnCancel As Button
+End Class
---
+++ FolderLocker/FolderLocker.vb
@@ -0,0 +1,48 @@
+﻿Imports System.IO
+Public Class frmFolderLocker
+    Public Property Locker As Object = $"{My.Computer.FileSystem.CurrentDirectory}\Locker"
+    Public Property Title As Object = "Folder Locker"
+    ' Make directory subroutine to be executed if locked folder does not exist
+    Private Sub MakeLockedFolder()
+        ' Create the folder
+        My.Computer.FileSystem.CreateDirectory(directory:=Locker)
+        MessageBox.Show(text:="Locker created successfully.", caption:=Title, buttons:=MessageBoxButtons.OK, icon:=vbInformation)
+    End Sub
+    ' Lock subroutine
+    Private Sub LockFolder()
+        ' Wait for user input
+        If MessageBox.Show(
+            text:="Are you sure you want to Lock the folder?", caption:=Title, buttons:=MessageBoxButtons.YesNo, icon:=vbQuestion) = vbYes Then
+            ' If user clicks yes, set hidden and system attributes
+            File.SetAttributes(path:=Locker, fileAttributes:=File.GetAttributes(path:=Locker) + 6)
+            MessageBox.Show(text:="Folder locked.", caption:=Title, buttons:=MessageBoxButtons.OK, icon:=vbInformation)
+        End If
+    End Sub
+    Private Sub FrmFolderLocker_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Hide()  ' hide the form at startup
+        If Not Directory.Exists(path:=Locker) Then  ' if folder doesn't exist
+            MakeLockedFolder()  ' create it
+            End ' we're done here
+        ElseIf Not File.GetAttributes(path:=Locker) And 6 Then  ' if folder exists and is unlocked
+            LockFolder()  ' lock it
+            End ' we're done here
+        Else    ' if folder exists and is locked
+            Show()  ' show the form to try to unlock the folder
+        End If
+    End Sub
+    ' if the user clicks OK
+    Private Sub BtnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        If txtPassword.Text = "Test" Then  ' if correct password
+            ' Clear hidden and system attributes
+            File.SetAttributes(path:=Locker, fileAttributes:=File.GetAttributes(path:=Locker) - 6)
+            MessageBox.Show(text:="Folder Unlocked successfully.", caption:=Title, buttons:=MessageBoxButtons.OK, icon:=vbInformation)
+        Else    ' if wrong password
+            MessageBox.Show(text:="Invalid password!", caption:=Title, buttons:=MessageBoxButtons.OK, icon:=vbCritical)
+        End If
+        End ' we're done here
+    End Sub
+
+    Private Sub BtnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        End ' we're done here
+    End Sub
+End Class
